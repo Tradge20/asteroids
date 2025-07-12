42e8cda0fb1580d1e914df7ae869319d5ec0de57
Tradge20 <haitiboi7969@gmail.com>
2025-07-12 14:38:06 -0400

    B: rotation and movement added to player

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from constants import *
+from player import Player
+from circleshape import CircleShape
 import pygame
 
 
@@ -6,15 +8,31 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    
+    
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
+        dt = clock.tick(60) / 1000.0
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatables.update(dt)   
+        for sprite in drawables:
+            sprite.draw(screen)
+            
+        
         pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
